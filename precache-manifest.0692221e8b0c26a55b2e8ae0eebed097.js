8f6979e5e26bce43ebd47873d2ad363cff5f9f1e
Михаил Охотников <mihailohotnikov@MacBook-Pro-Mihail.local>
2019-11-03 20:29:36 +0300

    Updates

---
+++ precache-manifest.0692221e8b0c26a55b2e8ae0eebed097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860fc85a112b2b51cae4717d75d8cdaf",
+    "url": "/re-store-react-app/index.html"
+  },
+  {
+    "revision": "07ecb6ccd06af12cf263",
+    "url": "/re-store-react-app/static/css/main.7c4f7045.chunk.css"
+  },
+  {
+    "revision": "6422b15f1943215377d6",
+    "url": "/re-store-react-app/static/js/2.5eca33b4.chunk.js"
+  },
+  {
+    "revision": "07ecb6ccd06af12cf263",
+    "url": "/re-store-react-app/static/js/main.d86fd989.chunk.js"
+  },
+  {
+    "revision": "25df2770310473ddc8eb",
+    "url": "/re-store-react-app/static/js/runtime-main.ad155d0f.js"
+  }
+]);
